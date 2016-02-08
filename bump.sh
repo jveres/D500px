05f2ab0369d5f3bcda0977b829f4fb74575db4de
jveres <janos.veres@gmail.com>
2016-02-08 22:39:40 +0100

    fixes

--- bump.sh
+++ bump.sh
@@ -1,5 +1,5 @@
 uno build Android --configuration=Release
-cp build/Android/Release/D500px.app D500px-debug.apk
+cp build/Android/Release/D500px.apk D500px-debug.apk
 git add .
 git commit -m "bump"
 git push origin master
